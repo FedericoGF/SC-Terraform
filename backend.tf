fc69f7821976d44404be3ce531b80f3304a72d06
FedericoGF <fedefrostylol@gmail.com>
2023-05-07 03:02:01 +0000

    Cambios en configuracion

--- backend.tf
+++ backend.tf
@@ -1,9 +1,15 @@
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "s3-bucket23ob"
-    dynamodb_table = "terraform-lock-table"
+    bucket = "s3-bucketfg"
+    dynamodb_table = "terraform-lock-table.s3"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
+
+
+
+
+
+
